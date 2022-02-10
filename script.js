e5a9f218377fc6c52f77e5b3cc28594f20af7b00
Nitay Kessner <nitayke@gmail.com>
2022-02-10 15:47:28 +0200

    .

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function whenLoaded() {
+    console.log('ok')
     document.getElementById("loader").hidden = true;
+    document.getElementById("loader-container").style
+    .setProperty("display", "none", "important");
     document.getElementById("container").hidden = false;
 }
